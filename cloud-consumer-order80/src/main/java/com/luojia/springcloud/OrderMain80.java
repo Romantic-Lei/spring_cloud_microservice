907e1585e52f4817f4c2d6ad7cab5caf57a9a6a9
Memories6 <1371910434@qq.com>
2020-03-18 23:34:39 +0800

    支付服务消费者

---
+++ cloud-consumer-order80/src/main/java/com/luojia/springcloud/OrderMain80.java
@@ -0,0 +1,11 @@
+package com.luojia.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
