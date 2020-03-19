53e0cc41106fbec17d90330857e539670b1b90e9
Memories6 <1371910434@qq.com>
2020-03-19 17:09:43 +0800

    服务消费者 轮询访问 服务提供者

--- cloud-consumer-order80/src/main/java/com/luojia/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/luojia/springcloud/controller/OrderController.java
@@ -16,7 +16,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class OrderController {
 
-    public static final String PAYMENT_URL="http://localhost:8001";
+    // public static final String PAYMENT_URL="http://localhost:8001";
+    public static final String PAYMENT_URL="http://CLOUD-PAYMENT-SERVICE";
     
     @Resource
     private RestTemplate restTemplate;
