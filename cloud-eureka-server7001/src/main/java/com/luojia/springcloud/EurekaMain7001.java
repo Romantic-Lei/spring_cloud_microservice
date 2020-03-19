3583b04425acfb5cf7adfa98f0dc674a44bd0a38
Memories6 <1371910434@qq.com>
2020-03-19 12:42:31 +0800

    eureka-单机版服务注册中心

---
+++ cloud-eureka-server7001/src/main/java/com/luojia/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.luojia.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册
+ * @author asus
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
