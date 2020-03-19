6bbc830063bf816aca3a8bb3036965073546fa7a
Memories6 <1371910434@qq.com>
2020-03-19 10:55:11 +0800

    支付模块重构

--- cloud-provider-payment8001/src/main/java/com/luojia/springcloud/controller/Controller.java
+++ cloud-provider-payment8001/src/main/java/com/luojia/springcloud/controller/Controller.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luojia.springcloud.entities.CommonResult;
@@ -21,7 +22,7 @@ public class Controller {
     private PaymentService paymentService;
     
     @PostMapping(value="payment/create")
-    public CommonResult create(Payment payment) {
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("******插入结果：" + result);
         
