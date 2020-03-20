f56c72918f363c2806970afc8c19a7ce4ead96e1
Memories6 <1371910434@qq.com>
2020-03-20 10:05:35 +0800

    consul 服务提供者

---
+++ cloud-providerconsul-payment8006/src/main/java/com/luojia/springcloud/PaymentMain8006.java
@@ -0,0 +1,12 @@
+package com.luojia.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain8006 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/com/luojia/springcloud/controller/PaymentController.java
@@ -0,0 +1,21 @@
+package com.luojia.springcloud.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/consul")
+    public String paymentZk() {
+        return "SpringCloud with zookeeper:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
