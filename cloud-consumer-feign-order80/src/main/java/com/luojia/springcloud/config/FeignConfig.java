decce991bc074ebe411b0bc35b250a6cf17580b8
Memories6 <1371910434@qq.com>
2020-03-20 23:01:40 +0800

    feign 日志打印

---
+++ cloud-consumer-feign-order80/src/main/java/com/luojia/springcloud/config/FeignConfig.java
@@ -0,0 +1,21 @@
+package com.luojia.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.Logger;
+
+@Configuration
+public class FeignConfig {
+
+    /**
+     * feignClient配置日志级别
+     *
+     * @return
+     */
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        // 请求和响应的头信息,请求和响应的正文及元数据
+        return Logger.Level.FULL;
+    }
+}
