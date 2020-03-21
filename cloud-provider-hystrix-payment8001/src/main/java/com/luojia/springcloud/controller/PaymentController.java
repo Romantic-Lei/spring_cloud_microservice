f97d1a973dce911ea564c55eefff4d37868615d9
Memories6 <1371910434@qq.com>
2020-03-21 12:03:57 +0800

    Hystrix断路器

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/luojia/springcloud/controller/PaymentController.java
@@ -0,0 +1,48 @@
+package com.luojia.springcloud.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luojia.springcloud.service.PaymentService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String servicePort;
+
+    /**
+     * 正常访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentInfo_OK(id);
+        log.info("*****result:" + result);
+        return result;
+    }
+
+    /**
+     * 超时访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentInfo_TimeOut(id);
+        log.info("*****result:" + result);
+        return result;
+    }
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/luojia/springcloud/service/PaymentService.java
@@ -0,0 +1,40 @@
+package com.luojia.springcloud.service;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+    /**
+     * 正常访问
+     *
+     * @param id
+     * @return
+     */
+    public String paymentInfo_OK(Integer id) {
+        return "线程池:" + Thread.currentThread().getName() + " paymentInfo_OK,id:" + id + "\t" + "O(∩_∩)O哈哈~";
+    }
+
+    /**
+     * 超时访问
+     * HystrixCommand:一旦调用服务方法失败并抛出了错误信息后,会自动调用@HystrixCommand标注好的fallbckMethod调用类中的指定方法
+     * execution.isolation.thread.timeoutInMilliseconds:线程超时时间3秒钟
+     *
+     * @param id
+     * @return
+     */
+    public String paymentInfo_TimeOut(Integer id) {
+        /// int age = 10 / 0;
+
+        int timeNumber = 2;
+        try {
+            // 暂停5秒钟
+            TimeUnit.SECONDS.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池:" + Thread.currentThread().getName() + " paymentInfo_TimeOut,id:" + id + "\t" + "O(∩_∩)O哈哈~  耗时(秒)"
+                + timeNumber;
+    }
+}
