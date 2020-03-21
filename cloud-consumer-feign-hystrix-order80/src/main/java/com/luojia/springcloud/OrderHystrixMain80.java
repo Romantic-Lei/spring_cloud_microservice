56397c389e2073d35da9c4c697e75a69b6492340
Memories6 <1371910434@qq.com>
2020-03-21 14:17:05 +0800

    指定全局兜底容错的方法

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/luojia/springcloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/luojia/springcloud/OrderHystrixMain80.java
@@ -2,10 +2,12 @@ package com.luojia.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
 public class OrderHystrixMain80 {
 
     public static void main(String[] args) {
--- cloud-consumer-feign-hystrix-order80/src/main/java/com/luojia/springcloud/controller/OrderHyrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/luojia/springcloud/controller/OrderHyrixController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luojia.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+// 定义一个全局兜底的容错方法
+@DefaultProperties(defaultFallback = "paymentInfo_Global_FallbackMethod")
 public class OrderHyrixController {
 
     @Resource
@@ -24,9 +29,21 @@ public class OrderHyrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+//    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+//    })
+//    未指定具体的容错方法，那么我们就找默认的兜底容错方法
+    @HystrixCommand
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
     }
-
+    
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "消费者，对方支付系统繁忙，请稍后重试";
+    }
+    
+    public String paymentInfo_Global_FallbackMethod() {
+        return "Global 异常处理信息，请稍后再试";
+    }
 }
