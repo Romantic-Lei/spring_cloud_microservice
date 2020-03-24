5dada0a6a62e84fc6d943bae0810b46a500cd26e
Memories6 <1371910434@qq.com>
2020-03-24 16:57:59 +0800

    sentinel 服务监控

--- cloud-provider-hystrix-payment8001/src/main/java/com/luojia/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/luojia/springcloud/service/PaymentService.java
@@ -64,7 +64,7 @@ public class PaymentService {
             fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
             @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),// 是否开启断路器
             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),// 请求次数
-            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),// 时间窗口期/时间范文
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),// 时间窗口期/时间范围
             @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")// 失败率达到多少后跳闸
     }
     )
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/luojia/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,27 @@
+package com.luojia.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA(){
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(800);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return "testA-----";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        log.info(Thread.currentThread().getName() + "...testB ");
+        return "testB   -----";
+    }
+}
