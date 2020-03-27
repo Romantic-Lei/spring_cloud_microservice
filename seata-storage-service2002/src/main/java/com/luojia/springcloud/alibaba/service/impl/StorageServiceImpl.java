fad7c98e9e328a0c114d2961f603b48f5d743ac5
Memories6 <1371910434@qq.com>
2020-03-27 11:09:20 +0800

    库存微服务系统

--- seata-storage-service2002/src/main/java/com/luojia/springcloud/alibaba/service/impl/StorageServiceImpl.java
+++ seata-storage-service2002/src/main/java/com/luojia/springcloud/alibaba/service/impl/StorageServiceImpl.java
@@ -2,9 +2,12 @@ package com.luojia.springcloud.alibaba.service.impl;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+
 import com.luojia.springcloud.alibaba.dao.StorageDao;
 import com.luojia.springcloud.alibaba.service.StorageService;
 
+@Service
 public class StorageServiceImpl implements StorageService {
 
     @Resource
